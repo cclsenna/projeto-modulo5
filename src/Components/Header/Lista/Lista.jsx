038f0a9079639f8ff8abdd1d9b83543375ed967f
unknown <rwps1234@gmail.com>
2022-05-30 19:28:01 -0300

    estilizando lista para menu responsivo

--- src/Components/Header/Lista/Lista.jsx
+++ src/Components/Header/Lista/Lista.jsx
@@ -1,9 +1,10 @@
+import styles from "./Lista.module.css"
 export default function Lista ({listas}) {
     return (
         <>
         {listas.map((lista,index) => (
             
-            <li key={index}>
+            <li className={styles.lista} key={index}>
                 <p>{lista}</p>
             </li>
         ))}
