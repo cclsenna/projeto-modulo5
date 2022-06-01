2c70d65eaf7fcf0aacdf470de0fe8b76de0d755e
Iata Anderson <96217918+IataAnderson@users.noreply.github.com>
2022-06-01 19:20:31 -0300

    estruturando AreadoGestor

---
+++ src/Components/AreadoGestor/AreadoGestor.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function AreadoGestor() {
+  return (
+    <main>
+        <img src="" alt="" />
+        <h1>Área do Gestor</h1>
+        <h2>Ainda não tem conta?<a>Cadastre-se.</a></h2>
+        <form>
+            <input placeholder='Digite seu melhor email' type="email" />
+            <input type="password" />
+            <button>Conecte-se</button>
+            <button>Conectar com o Google.</button>
+        </form>
+        <h2>Esqueceu sua senha? A gente te ajuda.</h2>
+    </main>
+  )
+}
+
+export default AreadoGestor
