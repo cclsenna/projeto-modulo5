ccc9bf95afa1a9f274a9c24089eb0393d624729a
unknown <rwps1234@gmail.com>
2022-05-31 23:10:16 -0300

    altereando de class para className

--- src/pages/Home/Carrossel/Carrossel.jsx
+++ src/pages/Home/Carrossel/Carrossel.jsx
@@ -2,32 +2,32 @@ import "./Carrossel.css";
 
 export default function Carrossel() {
   return (
-    <section class="carousel" aria-label="Gallery">
-      <ol class="carousel__viewport">
-        <li id="carousel__slide1" tabindex="0" class="carousel__slide">
-          <div class="carousel__snapper">
-            <a href="#carousel__slide3" class="carousel__prev">
+    <section className="carousel" aria-label="Gallery">
+      <ol className="carousel__viewport">
+        <li id="carousel__slide1" tabIndex="0" className="carousel__slide">
+          <div className="carousel__snapper">
+            <a href="#carousel__slide3" className="carousel__prev">
               Go to last slide
             </a>
-            <a href="#carousel__slide2" class="carousel__next">
+            <a href="#carousel__slide2" className="carousel__next">
               Go to next slide
             </a>
           </div>
-          <div class="container__titulo1">
+          <div className="container__titulo1">
             <h2>
               Festa Junina - Dia 27/06 - Quadra São Silvestre - Av Dias da Cruz
             </h2>
           </div>
         </li>
-        <li id="carousel__slide2" tabindex="0" class="carousel__slide">
-          <div class="carousel__snapper"></div>
-          <a href="#carousel__slide1" class="carousel__prev">
+        <li id="carousel__slide2" tabIndex="0" className="carousel__slide">
+          <div className="carousel__snapper"></div>
+          <a href="#carousel__slide1" className="carousel__prev">
             Go to previous slide
           </a>
-          <a href="#carousel__slide3" class="carousel__next">
+          <a href="#carousel__slide3" className="carousel__next">
             Go to next slide
           </a>
-          <div class="container__titulo2">
+          <div className="container__titulo2">
             <p>
               " Ensinar não é transferir conhecimento, mas criar as
               possibilidades para a sua própria produção ou a sua construção."
@@ -35,15 +35,15 @@ export default function Carrossel() {
             <h2>Paulo Freire</h2>
           </div>
         </li>
-        <li id="carousel__slide3" tabindex="0" class="carousel__slide">
-          <div class="carousel__snapper"></div>
-          <a href="#carousel__slide2" class="carousel__prev">
+        <li id="carousel__slide3" tabIndex="0" className="carousel__slide">
+          <div className="carousel__snapper"></div>
+          <a href="#carousel__slide2" className="carousel__prev">
             Go to previous slide
           </a>
-          <a href="#carousel__slide1" class="carousel__next">
+          <a href="#carousel__slide1" className="carousel__next">
             Go to next slide
           </a>
-          <div class="container__titulo3">
+          <div className="container__titulo3">
             <h2>
               Venha para a Escola Holandesa do Méier! Matriculas abertas até
               25/06
