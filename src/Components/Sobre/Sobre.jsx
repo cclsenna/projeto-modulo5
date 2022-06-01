7e3c9a212683289639c5e6dd6341810d4a3146ba
Iata Anderson <96217918+IataAnderson@users.noreply.github.com>
2022-06-01 18:37:08 -0300

    Estilo mobile pront

--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styles from './Sobre.module.css'
+import imgsobre from '../../assets/imgs/imgsobre.png'
 
 function Sobre() {
   return (
-    <main>
-        <h1 className={styles.title}>SOBRE</h1>        
-        <section>
-            <p className={styles.textoSobre}>
-            Há 35 anos, iniciamos nossa história como um curso preparatório para o vestibular. Hoje, além do Curso, atuamos também como Colégio nos segmentos de Educação Fundamental 1 e 2 e Ensino Médio, com 14 unidades no Rio de Janeiro e Niterói. Atualmente, fazemos parte do maior grupo de Educação Básica do Brasil.
-            </p>
+    <main className={styles.Conteudo}>
+        <h1 className={styles.title}>Sobre</h1>
+
+        <section className={styles.TextoImg}>
+            <article className={styles.textoSobre}>
+            Há 35 anos, iniciamos nossa história como um curso preparatório para o vestibular. 
+            Hoje, além do Curso, atuamos também como Colégio nos segmentos de Educação Fundamental 1 e 2 e Ensino Médio, com 14 unidades no Rio de Janeiro e Niterói. 
+            Atualmente, fazemos parte do maior grupo de Educação Básica do Brasil.
+            </article>        
+            <img className={styles.imgSobre} src={imgsobre} />
         </section>
-        <section></section>
     </main>
   )
 }
