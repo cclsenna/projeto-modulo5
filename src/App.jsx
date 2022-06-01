8112a9d4b1ab4afbce374673b49ecf871ec6bc1e
unknown <rwps1234@gmail.com>
2022-05-31 21:27:37 -0300

    importando componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
-import Header from './Components/Header/Header'
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
-
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
