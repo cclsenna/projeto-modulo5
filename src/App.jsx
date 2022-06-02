21c249569f965ba188f396c5f9a0c91d514279d1
unknown <rwps1234@gmail.com>
2022-06-02 16:21:15 -0300

    importando home

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import "./App.css";
 import Header from "./Components/Header/Header";
 import Home from "./pages/Home/Home";
-import styles from './App.module.css'
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer.jsx'
-import CardFunc from './Components/CardFunc/CardFunc.jsx';
-import imgCreate from './assets/imgs/img_Create.png'
-import imgSearch from './assets/imgs/img_search.svg'
-
+import Footer from "./Components/Footer/Footer.jsx";
+import CardFunc from "./Components/CardFunc/CardFunc.jsx";
+import imgCreate from "./assets/imgs/img_Create.png";
+import imgSearch from "./assets/imgs/img_search.svg";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer/>
     </div>
   );
 }
 
 export default App;
-
