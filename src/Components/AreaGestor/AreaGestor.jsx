7fd8fd39d5b61886b2e35929e0c7cff95f4d4e4e
Iata Anderson <96217918+IataAnderson@users.noreply.github.com>
2022-06-02 23:37:45 -0300

    Criando className

--- src/Components/AreaGestor/AreaGestor.jsx
+++ src/Components/AreaGestor/AreaGestor.jsx
@@ -5,22 +5,22 @@ import logoAreaGestor from '../../assets/imgs/logoAreaGestor.png'
 
 function AreaGestor() {
   return (
-   <main>
+   <main className={styles.containerAreaGestor}>
      <img className={styles.logoAreaGestor} src={logoAreaGestor} />
      <h1 className={styles.titleAreaGestor}>Acesse área do gestor</h1>
      <h2 className={styles.subTitleAreaGestor}> Ainda não tem conta? <a href="#">Cadastre-se</a></h2>
      <form className={styles.formsAreaGestor}>
        <label className={styles.labelAreaGestor}>
        <input className={styles.inputEmail} type="email" name='email' placeholder='Seu melhor e-mail' />
-       <input  className={styles.inputSenha} type="password" name='password' placeholder='Digite sua senha'/>
+       <input  className={styles.inputSenha} type="password" name='senha' placeholder='Digite sua senha'/>
        </label>
-        <section>
-       <button>Conecte-se</button>
-    <button>Conectar com o Google</button>
+        <section className={styles.btnAreaGestor}>
+       <button className={styles.btnConectese}>Conecte-se</button>
+    <button className={styles.btnConectar-google}>Conectar com o Google</button>
        </section>    
     
      </form>
-     <h2>Esqueceu sua senha? <a href="#">A gente te ajuda.</a></h2>
+     <h2 className={styles.subtitleEsqueceu}>Esqueceu sua senha? <a href="#">A gente te ajuda.</a></h2>
    </main>
     
     
