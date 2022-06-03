a36a04596bb8a4f8f37045e7eb44afa3497a7846
Caio Senna <caiosenna93@gmail.com>
2022-06-02 22:56:27 -0300

    montagem inicial e funcional da página da área logada do gestor

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Header from "./Components/Header/Header";
 import Home from "./pages/Home/Home";
 import Footer from "./Components/Footer/Footer.jsx";
-import CardFunc from "./Components/CardFunc/CardFunc.jsx";
+import CardFunc from "./pages/DashBoardEscolha/CardFunc/CardFunc.jsx";
 import imgCreate from "./assets/imgs/img_Create.png";
 import imgSearch from "./assets/imgs/img_search.svg";
+import DashboardEscolha from "./pages/DashBoardEscolha/DashboardEscolha";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.container}>
       <Header />
-      <Home />
+      {/* <Home /> */}
+      <DashboardEscolha/>
       <Footer />
     </div>
   );
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import Home from "../pages/Home/Home.jsx";
+import DashboardEscolha from "../pages/DashBoardEscolha/DashboardEscolha.jsx";
+
+
+const Routes=()=>{
+
+
+}
+
+export default Routes;
