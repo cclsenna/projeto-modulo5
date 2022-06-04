fe566dd484e23047889cbd1fa1e6e220331dd29c
Caio Senna <caiosenna93@gmail.com>
2022-06-03 23:25:09 -0300

    iniciando configuracao da busca

--- src/pages/protected/DashboardBusca/DashboardBusca.jsx
+++ src/pages/protected/DashboardBusca/DashboardBusca.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputBusca from './InputBusca/InputBusca';
 import styles from './DashboardBusca.module.css';
 
 
 function DashboardBusca() {
+
+    const [busca,setBusca]=useState('');
+
   return (
     <div className={styles.container}>
+        <h1>Busca e Edição</h1>
         <InputBusca textoPlaceholder="Buscar aluno"/>
+        <ul>
+            
+        </ul>
     </div>
   )
 }
